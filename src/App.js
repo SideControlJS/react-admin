e0cec73ec3421ae4e294eb17cd5a3883e7e629a4
Jesse <jesse.lare@gmail.com>
2023-12-31 18:13:21 -0500

    initial commit after changing repos...

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+function App() {
+  return (
+    <div className="app">
+
+    </div>
+  );
+}
+
+export default App;
