afd6e31da96e66a9fa86d5c86a90656779cbb431
Jesse <jesse.lare@gmail.com>
2023-12-31 21:38:28 -0500

    updating sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -19,6 +19,31 @@ import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 
 
 const Sidebar = () => {
-    return <div>Sidebar</div>
-}
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setisCollapsed] = useState(false);
+    const [selected, setSelected] = useState("Dashboard");
+
+    return (
+        <Box> 
+            sx={{
+                "& .pro-sidebar": {
+                    background: `${colors.primary[400]} !important`,
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important",
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important",
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important",
+                },
+                "& .pro-menu-item.active": {
+                    color: "#6870fa !important",
+                },}
+            }
+            </Box>
+        );
+};
     export default Sidebar;
