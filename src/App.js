7869b2fc5ed49279ba2f008fa1d18ab307ae7b1d
Jesse <jesse.lare@gmail.com>
2023-12-31 20:44:26 -0500

    completing Topbar with search bar and light/dark mode functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar  from "./scenes/global/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,11 +10,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content">  </main>
+          <main className="content">
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
-    </ColorModeContext.Provider>
-    
+    </ColorModeContext.Provider>   
   );
 }
 
